be261e993061f817aa5402487fcfb04c72d7f6d2
Robotteve <60617472+Robotteve@users.noreply.github.com>
2020-03-02 17:07:21 +0100

    1. változat. ---

---
+++ src/udvozlet/Udvozlet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udvozlet;
+
+/**
+ *
+ * @author Erik
+ */
+public class Udvozlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static void metodus1(){
+        System.out.println("Üdvözöllek!");
+    }
+    
+    public static void main(String[] args) {
+        metodus1();
+    }
+    
+}
